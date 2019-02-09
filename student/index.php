cdeefff776d43e0156b09730ff97d6360f5df01c
Tushar <garg.tusharmnnit@gmail.com>
2019-02-10 00:34:41 +0530

    CSS and student index code

---
+++ student/index.php
@@ -0,0 +1,61 @@
+<?php
+
+ob_start();
+session_start();
+
+if($_SESSION['name']!='oasis')
+{
+  header('location: ../index.php');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- head started -->
+<head>
+<title>Online Attendance Management System 1.0</title>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="../css/main.css">
+
+</head>
+<!-- head ended -->
+
+<!-- body started -->
+<body>
+
+<!-- Menus started-->
+<header>
+
+  <h1>Online Attendance Management System 1.0</h1>
+  <div class="navbar">
+  <a href="index.php">Home</a>
+  <a href="students.php">Students</a>
+  <a href="report.php">My Report</a>
+  <a href="account.php">My Account</a>
+  <a href="../logout.php">Logout</a>
+
+</div>
+
+</header>
+<!-- Menus ended -->
+
+<center>
+
+<!-- Content, Tables, Forms, Texts, Images started -->
+<div class="row">
+    <div class="content">
+      <p>Be attentive and be regular :)</p>
+    <img src="../img/tcr.png" height="200px" width="300px" />
+
+  </div>
+
+</div>
+<!-- Contents, Tables, Forms, Images ended -->
+
+</center>
+
+</body>
+<!-- Body ended  -->
+
+</html>
