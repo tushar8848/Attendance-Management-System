6a586f0f8b0853e83df1429ec8d10a7640081661
Tushar <garg.tusharmnnit@gmail.com>
2019-02-09 12:10:28 +0530

    Admin utilities and signup html added

--- admin/index.php
+++ admin/index.php
@@ -43,3 +43,169 @@ include('connect.php');
 
 
 
+<!DOCTYPE html>
+<html lang="en">
+<!-- head started -->
+<head>
+<title>Online Attendance Management System 1.0</title>
+<meta charset="UTF-8">
+
+  <link rel="stylesheet" type="text/css" href="../css/main.css">
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+   
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" >
+   
+  <link rel="stylesheet" href="styles.css" >
+   
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<style type="text/css">
+
+  .message{
+    padding: 10px;
+    font-size: 15px;
+    font-style: bold;
+    color: black;
+  }
+</style>
+</head>
+<!-- head ended -->
+
+<!-- body started -->
+<body>
+
+    <!-- Menus started-->
+    <header>
+
+      <h1>Online Attendance Management System 1.0</h1>
+      <div class="navbar">
+      <a href="signup.php">Create Users</a>
+      <a href="index.php">Add Data</a>
+      <a href="../logout.php">Logout</a>
+
+    </div>
+
+    </header>
+    <!-- Menus ended -->
+
+<center>
+<!-- Error or Success Message printint started -->
+<div class="message">
+        <?php if(isset($success_msg)) echo $success_msg; if(isset($error_msg)) echo $error_msg; ?>
+</div>
+<!-- Error or Success Message printint ended -->
+
+<!-- Content, Tables, Forms, Texts, Images started -->
+<div class="content">
+
+  <center> Select: <a href="#teacher">Teacher</a> | <a href="">Student</a> <br></center>
+
+  <div class="row" id="student">
+
+
+
+      <form method="post" class="form-horizontal col-md-6 col-md-offset-3">
+      <h4>Add Student's Information</h4>
+      <div class="form-group">
+          <label for="input1" class="col-sm-3 control-label">Reg. No.</label>
+          <div class="col-sm-7">
+            <input type="text" name="st_id"  class="form-control" id="input1" placeholder="student reg. no." />
+          </div>
+      </div>
+
+      <div class="form-group">
+          <label for="input1" class="col-sm-3 control-label">Name</label>
+          <div class="col-sm-7">
+            <input type="text" name="st_name"  class="form-control" id="input1" placeholder="student full name" />
+          </div>
+      </div>
+
+      <div class="form-group">
+          <label for="input1" class="col-sm-3 control-label">Department</label>
+          <div class="col-sm-7">
+            <input type="text" name="st_dept"  class="form-control" id="input1" placeholder="department ex. CSE" />
+          </div>
+      </div>
+
+      <div class="form-group">
+          <label for="input1" class="col-sm-3 control-label">Batch</label>
+          <div class="col-sm-7">
+            <input type="text" name="st_batch"  class="form-control" id="input1" placeholder="batch e.x 2020" />
+          </div>
+      </div>
+
+      <div class="form-group">
+          <label for="input1" class="col-sm-3 control-label">Semester</label>
+          <div class="col-sm-7">
+            <input type="text" name="st_sem"  class="form-control" id="input1" placeholder="semester ex. Fall-15" />
+          </div>
+      </div>
+
+      <div class="form-group">
+          <label for="input1" class="col-sm-3 control-label">Email</label>
+          <div class="col-sm-7">
+            <input type="email" name="st_email"  class="form-control" id="input1" placeholder="valid email" />
+          </div>
+      </div>
+
+
+      <input type="submit" class="btn btn-primary col-md-2 col-md-offset-8" value="Add Student" name="std" />
+    </form>
+
+  </div>
+<br><br><br>
+  <div class="rowtwo" id="teacher">
+  
+
+       <form method="post" class="form-horizontal col-md-6 col-md-offset-3">
+        <h4>Add Teacher's Information</h4>
+      <div class="form-group">
+          <label for="input1" class="col-sm-3 control-label">Teacher ID</label>
+          <div class="col-sm-7">
+            <input type="text" name="tc_id"  class="form-control" id="input1" placeholder="teacher's id" />
+          </div>
+      </div>
+
+      <div class="form-group">
+          <label for="input1" class="col-sm-3 control-label">Name</label>
+          <div class="col-sm-7">
+            <input type="text" name="tc_name"  class="form-control" id="input1" placeholder="teacher full name" />
+          </div>
+      </div>
+
+      <div class="form-group">
+          <label for="input1" class="col-sm-3 control-label">Department</label>
+          <div class="col-sm-7">
+            <input type="text" name="tc_dept"  class="form-control" id="input1" placeholder="department ex. CSE" />
+          </div>
+      </div>
+
+      <div class="form-group">
+          <label for="input1" class="col-sm-3 control-label">Email</label>
+          <div class="col-sm-7">
+            <input type="email" name="tc_email"  class="form-control" id="input1" placeholder="valid email" />
+          </div>
+      </div>
+
+      <div class="form-group">
+          <label for="input1" class="col-sm-3 control-label">Subject Name</label>
+          <div class="col-sm-7">
+            <input type="text" name="tc_course"  class="form-control" id="input1" placeholder="subject ex. Software Engineering" />
+          </div>
+      </div>
+
+      <input type="submit" class="btn btn-primary col-md-2 col-md-offset-8" value="Add Teacher" name="tcr" />
+    </form>
+    
+  </div>
+
+
+</div><br>
+<!-- Contents, Tables, Forms, Images ended -->
+
+</center>
+</body>
+<!-- Body ended  -->
+</html>
--- admin/signup.php
+++ admin/signup.php
@@ -56,3 +56,123 @@ include('connect.php');
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- head started -->
+<head>
+<title>Online Attendance Management System 1.0</title>
+<meta charset="UTF-8">
+
+  <link rel="stylesheet" type="text/css" href="../css/main.css">
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+   
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" >
+   
+  <link rel="stylesheet" href="styles.css" >
+   
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+<!-- head ended -->
+
+<!-- body started -->
+<body>
+
+    <!-- Menus started-->
+    <header>
+
+      <h1>Online Attendance Management System 1.0</h1>
+      <div class="navbar">
+      <a href="signup.php">Create Users</a>
+      <a href="index.php">Add Data</a>
+      <a href="../logout.php">Logout</a>
+
+    </div>
+
+    </header>
+    <!-- Menus ended -->
+
+<center>
+<h1>Create User</h1>
+<p>    <?php
+    if(isset($success_msg)) echo $success_msg;
+    if(isset($error_msg)) echo $error_msg;
+     ?>
+       
+     </p>
+     <br>
+<div class="content">
+
+  <div class="row">
+   
+    <form method="post" class="form-horizontal col-md-6 col-md-offset-3">
+
+      <div class="form-group">
+          <label for="input1" class="col-sm-3 control-label">Email</label>
+          <div class="col-sm-7">
+            <input type="text" name="email"  class="form-control" id="input1" placeholder="your email" />
+          </div>
+      </div>
+
+      <div class="form-group">
+          <label for="input1" class="col-sm-3 control-label">Username</label>
+          <div class="col-sm-7">
+            <input type="text" name="uname"  class="form-control" id="input1" placeholder="choose username" />
+          </div>
+      </div>
+
+      <div class="form-group">
+          <label for="input1" class="col-sm-3 control-label">Password</label>
+          <div class="col-sm-7">
+            <input type="password" name="pass"  class="form-control" id="input1" placeholder="choose a strong password" />
+          </div>
+      </div>
+
+      <div class="form-group">
+          <label for="input1" class="col-sm-3 control-label">Full Name</label>
+          <div class="col-sm-7">
+            <input type="text" name="fname"  class="form-control" id="input1" placeholder="your full name" />
+          </div>
+      </div>
+
+      <div class="form-group">
+          <label for="input1" class="col-sm-3 control-label">Phone Number</label>
+          <div class="col-sm-7">
+            <input type="text" name="phone"  class="form-control" id="input1" placeholder="your phone number" />
+          </div>
+      </div>
+
+
+      <div class="form-group" class="radio">
+      <label for="input1" class="col-sm-3 control-label">Role</label>
+      <div class="col-sm-7">
+        <label>
+          <input type="radio" name="type" id="optionsRadios1" value="student" checked> Student
+        </label>
+            <label>
+          <input type="radio" name="type" id="optionsRadios1" value="teacher"> Teacher
+        </label>
+        <label>
+          <input type="radio" name="type" id="optionsRadios1" value="admin"> Admin
+        </label>
+      </div>
+      </div>
+
+      <input type="submit" class="btn btn-primary col-md-2 col-md-offset-8" value="Signup" name="signup" />
+    </form>
+  </div>
+    <br>
+    <p><strong>Already have an account? <a href="../index.php">Login</a> here.</strong></p>
+
+</div>
+
+</center>
+
+</body>
+<!-- Body ended  -->
+
+</html>
