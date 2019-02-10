03b61e1c426d389bd98169f343b90008969a05c7
Tushar <garg.tusharmnnit@gmail.com>
2019-02-11 01:14:25 +0530

    teachers dashboard

---
+++ teacher/connect.php
@@ -0,0 +1,6 @@
+<?php
+
+mysql_connect('localhost','root','') or die('Cannot connect to server');
+mysql_select_db('attsystem') or die ('Cannot found database');
+
+?>
---
+++ teacher/index.php
@@ -0,0 +1,51 @@
+<?php
+
+ob_start();
+session_start();
+
+if($_SESSION['name']!='oasis')
+{
+  header('location: ../index.php');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Online Attendance Management System 1.0</title>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="../css/main.css">
+
+</head>
+<body>
+
+<header>
+
+  <h1>Online Attendance Management System 1.0</h1>
+  <div class="navbar">
+  <a href="index.php">Home</a>
+  <a href="students.php">Students</a>
+  <a href="teachers.php">Faculties</a>
+  <a href="attendance.php">Attendance</a>
+  <a href="report.php">Report</a>
+  <a href="../logout.php">Logout</a>
+
+</div>
+
+</header>
+
+<center>
+
+<div class="row">
+    <div class="content">
+      <p>One step solution for your class room :)</p>
+    <img src="../img/tcr.png" height="200px" width="300px" />
+
+  </div>
+
+</div>
+
+</center>
+
+</body>
+</html>
