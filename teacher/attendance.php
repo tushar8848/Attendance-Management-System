348f624f3d4f5fead16569aa913be1ff1219ab67
Tushar <garg.tusharmnnit@gmail.com>
2019-02-11 02:35:58 +0530

    attendance adding page

--- teacher/attendance.php
+++ teacher/attendance.php
@@ -38,3 +38,136 @@ if($_SESSION['name']!='oasis')
   }
  ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Online Attendance Management System 1.0</title>
+<meta charset="UTF-8">
+
+  <link rel="stylesheet" type="text/css" href="../css/main.css">
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+   
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" >
+   
+  <link rel="stylesheet" href="styles.css" >
+   
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+<style type="text/css">
+  .status{
+    font-size: 10px;
+  }
+
+</style>
+
+</head>
+<body>
+
+<header>
+
+  <h1>Online Attendance Management System 1.0</h1>
+  <div class="navbar">
+  <a href="index.php">Home</a>
+  <a href="students.php">Students</a>
+  <a href="teachers.php">Faculties</a>
+  <a href="attendance.php">Attendance</a>
+  <a href="report.php">Report</a>
+  <a href="../logout.php">Logout</a>
+
+</div>
+
+</header>
+
+<center>
+
+<div class="row">
+
+  <div class="content">
+    <h3>Attendance of <?php echo date('Y-m-d'); ?></h3>
+    <br>
+
+    <center><p><?php if(isset($att_msg)) echo $att_msg; if(isset($error_msg)) echo $error_msg; ?></p></center> 
+    
+    <form action="" method="post" class="form-horizontal col-md-6 col-md-offset-3">
+
+     <div class="form-group">
+
+               <!-- <label>Select Batch</label>
+                
+                <select name="whichbatch" id="input1">
+                      <option name="eight" value="38">38</option>
+                      <option name="seven" value="37">37</option>
+                </select>-->
+
+
+                <label>Enter Batch</label>
+                <input type="text" name="whichbatch" id="input2" placeholder="Only 2020">
+              </div>
+               
+     <input type="submit" class="btn btn-primary col-md-2 col-md-offset-5" value="Show!" name="batch" />
+
+    </form>
+
+    <div class="content"></div>
+    <form action="" method="post">
+
+      <div class="form-group">
+
+        <label >Select Subject</label>
+              <select name="whichcourse" id="input1">
+              <option  value="algo">Analysis of Algorithms</option>
+         <option  value="algolab">Analysis of Algorithms Lab</option>
+        <option  value="dbms">Database Management System</option>
+        <option  value="dbmslab">Database Management System Lab</option>
+        <option  value="weblab">Web Programming Lab</option>
+        <option  value="os">Operating System</option>
+        <option  value="oslab">Operating System Lab</option>
+        <option  value="obm">Object Based Modeling</option>
+        <option  value="softcomp">Soft Computing</option>
+              </select>
+
+      </div>
+
+    <table class="table table-stripped">
+      <thead>
+        <tr>
+          <th scope="col">Reg. No.</th>
+          <th scope="col">Name</th>
+          <th scope="col">Department</th>
+          <th scope="col">Batch</th>
+          <th scope="col">Semester</th>
+          <th scope="col">Email</th>
+          <th scope="col">Status</th>
+        </tr>
+      </thead>
+   <?php
+
+    if(isset($_POST['batch'])){
+
+     $i=0;
+     $radio = 0;
+     $batch = 2020;
+     $all_query = mysql_query("select * from students where st_batch='$batch' order by st_id asc");
+
+    
+  }
+      ?>
+    </table>
+
+    <center><br>
+    <input type="submit" class="btn btn-primary col-md-2 col-md-offset-10" value="Save!" name="att" />
+  </center>
+
+</form>
+  </div>
+
+</div>
+
+</center>
+
+</body>
+</html>
