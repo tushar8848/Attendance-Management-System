8fccb709c44012573ff202140f371984d6cd6803
Tushar <garg.tusharmnnit@gmail.com>
2019-02-11 19:18:36 +0530

    student individual report

---
+++ teacher/report.php
@@ -0,0 +1,156 @@
+<?php
+
+ob_start();
+session_start();
+
+if($_SESSION['name']!='oasis')
+{
+  header('location: login.php');
+}
+?>
+<?php include('connect.php');?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Online Attendance Management System 1.0</title>
+<meta charset="UTF-8">
+
+  <link rel="stylesheet" type="text/css" href="../css/main.css">
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+   
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" >
+   
+  <link rel="stylesheet" href="styles.css" >
+   
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+<header>
+
+  <h1>Online Attendance Management System 1.0</h1>
+  <div class="navbar">
+  <a href="index.php">Home</a>
+  <a href="students.php">Students</a>
+  <a href="teachers.php">Faculties</a>
+  <a href="attendance.php">Attendance</a>
+  <a href="report.php">Report</a>
+  <a href="../logout.php">Logout</a>
+
+</div>
+
+</header>
+
+<center>
+
+<div class="row">
+
+  <div class="content">
+    <h3>Individual Report</h3>
+
+    <form method="post" action="">
+
+    <label>Select Subject</label>
+    <select name="whichcourse">
+    <option  value="algo">Analysis of Algorithms</option>
+         <option  value="algolab">Analysis of Algorithms Lab</option>
+        <option  value="dbms">Database Management System</option>
+        <option  value="dbmslab">Database Management System Lab</option>
+        <option  value="weblab">Web Programming Lab</option>
+        <option  value="os">Operating System</option>
+        <option  value="oslab">Operating System Lab</option>
+        <option  value="obm">Object Based Modeling</option>
+        <option  value="softcomp">Soft Computing</option>
+    </select>
+
+      <p>  </p>
+      <label>Student Reg. No.</label>
+      <input type="text" name="sr_id">
+      <input type="submit" name="sr_btn" value="Go!" >
+
+    </form>
+
+    <h3>Mass Report</h3> 
+    <br>
+
+    <br>
+
+   <?php
+
+    if(isset($_POST['sr_btn'])){
+
+     $sr_id = $_POST['sr_id'];
+     $course = $_POST['whichcourse'];
+
+     $single = mysql_query("select stat_id,count(*) as countP from attendance where attendance.stat_id='$sr_id' and attendance.course = '$course' and attendance.st_status='Present'");
+      $singleT= mysql_query("select count(*) as countT from attendance where attendance.stat_id='$sr_id' and attendance.course = '$course'");
+    //  $count_tot = mysql_num_rows($singleT);
+  } 
+
+    if(isset($_POST['sr_date'])){
+
+     $sdate = $_POST['date'];
+     $course = $_POST['course'];
+
+     $all_query = mysql_query("select * from attendance where reports.stat_date='$sdate' and reports.course = '$course'");
+
+    }
+    if(isset($_POST['sr_date'])){
+
+      ?>
+
+    <table class="table table-stripped">
+      <thead>
+        <tr>
+          <th scope="col">Reg. No.</th>
+          <th scope="col">Name</th>
+          <th scope="col">Department</th>
+          <th scope="col">Batch</th>
+          <th scope="col">Date</th>
+          <th scope="col">Attendance Status</th>
+        </tr>
+     </thead>
+
+
+    <?php
+
+     $i=0;
+     while ($data = mysql_fetch_array($all_query)) {
+
+       $i++;
+
+     ?>
+        <tbody>
+           <tr>
+             <td><?php echo $data['st_id']; ?></td>
+             <td><?php echo $data['st_name']; ?></td>
+             <td><?php echo $data['st_dept']; ?></td>
+             <td><?php echo $data['st_batch']; ?></td>
+             <td><?php echo $data['stat_date']; ?></td>
+             <td><?php echo $data['st_status']; ?></td>
+           </tr>
+        </tbody>
+
+     <?php 
+   } 
+  }
+     ?>
+     
+    </table>
+
+
+   
+
+  </div>
+
+</div>
+
+</center>
+
+</body>
+</html>
---
+++ teacher/students.php
@@ -0,0 +1,114 @@
+<?php
+
+ob_start();
+session_start();
+
+if($_SESSION['name']!='oasis')
+{
+  header('location: login.php');
+}
+?>
+<?php include('connect.php');?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Online Attendance Management System 1.0</title>
+<meta charset="UTF-8">
+  <link rel="stylesheet" type="text/css" href="../css/main.css">
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+   
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" >
+   
+  <link rel="stylesheet" href="styles.css" >
+   
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</style>
+
+</head>
+<body>
+
+<header>
+
+  <h1>Online Attendance Management System 1.0</h1>
+  <div class="navbar">
+  <a href="index.php">Home</a>
+  <a href="students.php">Students</a>
+  <a href="teachers.php">Faculties</a>
+  <a href="attendance.php">Attendance</a>
+  <a href="report.php">Report</a>
+  <a href="../logout.php">Logout</a>
+
+
+</div>
+
+</header>
+
+<center>
+
+<div class="row">
+
+  <div class="content">
+    <h3>Student List</h3>
+    <br>
+    <form method="post" action="">
+      <label>Batch (ex. 2020)</label>
+      <input type="text" name="sr_batch">
+      <input type="submit" name="sr_btn" value="Go!" >
+    </form>
+    <br>
+    <table class="table table-stripped">
+      <thead>
+        <tr>
+          <th scope="col">Registration No.</th>
+          <th scope="col">Name</th>
+          <th scope="col">Department</th>
+          <th scope="col">Batch</th>
+          <th scope="col">Semester</th>
+          <th scope="col">Email</th>
+        </tr>
+      </thead>
+
+   <?php
+
+    if(isset($_POST['sr_btn'])){
+     
+     $srbatch = $_POST['sr_batch'];
+     $i=0;
+     
+     $all_query = mysql_query("select * from students where students.st_batch = '$srbatch' order by st_id asc ");
+     
+     while ($data = mysql_fetch_array($all_query)) {
+       $i++;
+     
+     ?>
+  <tbody>
+     <tr>
+       <td><?php echo $data['st_id']; ?></td>
+       <td><?php echo $data['st_name']; ?></td>
+       <td><?php echo $data['st_dept']; ?></td>
+       <td><?php echo $data['st_batch']; ?></td>
+       <td><?php echo $data['st_sem']; ?></td>
+       <td><?php echo $data['st_email']; ?></td>
+     </tr>
+  </tbody>
+
+     <?php 
+          } 
+              }
+      ?>
+      
+    </table>
+
+  </div>
+
+</div>
+
+</center>
+
+</body>
+</html>
